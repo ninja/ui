2e0139884f50235811e39a390dff5c8023c08c19
Jamie Hoover <dont.tase@me.com>
2010-08-07 13:19:34 -0700

    Fixes css path in Sinatra helper.

--- lib/sinatra/ninjaui.rb
+++ lib/sinatra/ninjaui.rb
@@ -4,7 +4,7 @@ require 'sinatra/base'
 module Sinatra
   module NinjauiHelpers
     def ninjaui
-      '<link href="/public/ninjaui/webjutsu.css?' + Ninjaui::VERSION + '" media="all" rel="stylesheet" type="text/css" />' + "\n    " +
+      '<link href="/ninjaui/webjutsu.css?' + Ninjaui::VERSION + '" media="all" rel="stylesheet" type="text/css" />' + "\n    " +
       '<script src="/ninjaui/webjutsu.js?' + Ninjaui::VERSION + '" type="text/javascript"> </script>'
     end
   end
