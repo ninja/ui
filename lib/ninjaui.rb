94304af66ce09c005163dbab03841b2f339ce701
Jamie Hoover <dont.tase@me.com>
2011-02-14 15:33:26 -0800

    Removes versions from source file names. Appends version to files at build time.

--- lib/ninjaui.rb
+++ lib/ninjaui.rb
@@ -22,21 +22,23 @@ module Ninjaui
       destination = "#{ARGV[0] ||= 'public'}/#{NAME}"
 
       # Remove outdated files
-      FileUtils.rm_rf destination# unless !File.exists?(destination)
+      FileUtils.rm_rf destination
 
       # Copy new files
-      FileUtils.cp_r origin, destination
+      FileUtils.mkdir_p destination + '/images'
+      FileUtils.cp_r origin + '/images', destination
+      FileUtils.cp origin + '/jquery.' + NAME + '.js', destination + '/jquery.' + NAME + '-' + VERSION + '.js'
+      FileUtils.cp origin + '/' + NAME + '.css', destination + '/' + NAME + '-' + VERSION + '.css'
 
       # Compile JavaScript file
       File.open(destination + '/jquery.' + NAME + '-' + VERSION + '.min.js', 'w') {|f|
         f.write(Closure::Compiler.new.compile(
-          File.open(origin + '/jquery.' + NAME + '-' + VERSION + '.js', 'r')
+          File.open(origin + '/jquery.' + NAME + '.js', 'r')
         ))
       }
 
       puts "#{SUMMARY} installed here: #{destination}"
     end
-
   end
 
 end
