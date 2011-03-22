fbe3f2339fbf81ce4d5411f96bf604e75aaa1519
Jamie Hoover <dont.tase@me.com>
2011-03-22 08:52:08 -0700

    Removes outdated reference files.

--- old/javascripts/jquery.ninjaui.navigation.js
+++
@@ -1,9 +0,0 @@
-$.fn.ninjaNavigationCreate = function(customOptions) {
-  var options = $.extend({
-    colors:$.ninjaColorsGet(),
-    radius:$.ninjaRadius
-  }, customOptions);
-  return this.each(function(i, navigation){
-    $(navigation).addClass('ninjaPlastic ninjaNavigation').css(options.colors.foreground).ninjaRadius({radius:options.radius});
-  });
-};
