0c1a29df3eec87b3413cc001b52061c6f1cce170
Faisal N. Jawdat <faisal@faisal.com>
2011-04-06 17:44:07 -0700

    () for .ninja

--- test/test-automated.js
+++ test/test-automated.js
@@ -3,6 +3,6 @@ $versions('1.5', '1.5.1', '1.5.2').load('../lib/jquery.ninja.ui.js').execute(fun
 
   test('should load jQuery and Ninja UI.', function() {
     equal(jQuery.fn.jquery, version, 'jQuery ' + version + ' loaded.');
-    ok($.ninja, 'Ninja UI loaded.');
+    ok($.ninja(), 'Ninja UI loaded.');
   });
 });
