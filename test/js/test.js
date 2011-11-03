1b9520744820f386804ba48f10dcfdaebe7f0856
Jamie Hoover ⚔ <dont.tase@me.com>
2011-11-03 16:01:58 -0700

    Remove submodules
    Brings latest test and themes code into the repo and removes submodule dependencies.

---
+++ test/js/test.js
@@ -0,0 +1,41 @@
+/*globals
+  $versions, test, equal, ok, expect, module
+*/
+
+/*jshint
+  bitwise: true, browser: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 2, jquery: true, newcap: true, noarg: true, noempty: true, nomen: true, nonew: true, regexp: true, strict: true, undef: true, white: false
+*/
+
+$versions('1.6.4', '1.6.3', '1.6.2', '1.6.1', '1.6', '1.5.2', '1.5.1', '1.5').load('../jquery.ninjaui.js').execute(function ($, jQuery, version) {
+
+  'use strict';
+
+  module('Infrastructure with jQuery ' + version);
+
+    test('should load jQuery and Ninja UI', function () {
+      expect(2);
+      equal(jQuery.fn.jquery, version, 'jQuery');
+      ok($.ninja(), 'Ninja UI');
+    });
+
+
+  module('.button()');
+
+    test("should have Ninja UI's default class", function () {
+      expect(1);
+      ok($.ninja().button().hasClass('ninja'), '.ninja');
+    });
+
+    test("should accept css overrides on creation", function () {
+      expect(1);
+      equal($.ninja().button({css: {margin: '20em'}}).css('margin'), '20em', "making them visible in jQuery's .css()");
+      // Note that different browsers are not consistent in how they deal with invalid styles.
+    });
+
+    test("should accept html content on creation", function () {
+      expect(1);
+      $('#qunit-fixture').append($.ninja().button({html: 'New Button'}));
+      equal($("#qunit-fixture .ninja").text(), "New Button", "which jQuery can then render");
+    });
+
+});
