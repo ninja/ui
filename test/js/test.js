177a125d5a5cbbbd2b049d268e36e3c9f3f14787
Faisal N. Jawdat <faisal@faisal.com>
2011-11-04 17:29:29 -0400

    converted tests to specs

--- test/js/test.js
+++ test/js/test.js
@@ -1,5 +1,5 @@
 /*globals
-  QUnit, $versions, test, equal, ok, expect, module
+  QUnit, $versions, describe, before, after, given, it, assert
 */
 
 /*jshint
@@ -19,86 +19,100 @@ else {
 
 QUnit.config.hidepassed = true;
 QUnit.config.reorder = true;
+QUnit.specify.globalApi = true;
 
-$versions(jQueryVersions).load(scriptPath).execute(function ($, jQuery, version) {
-
+$versions(jQueryVersions).load(scriptPath).execute(function($, jQuery, version) {
   'use strict';
-
-  module('Load with jQuery ' + version);
-
-    test('should load jQuery and Ninja UI', function () {
-      expect(2);
-      equal($.fn.jquery, version, 'jQuery');
-      ok($.ninja(), 'Ninja UI');
-    });
-
-    if (development) {
-      test('should return Ninja UI version', function () {
-        expect(1);
-        equal($.ninja().version(), 'development', 'Ninja UI version: development');
-      });
-    }
 
   var $examples = $('<div class="ninjaui-examples"><div class="ninjaui-examples-title">jQuery ' + version + ' Examples</div></div>').appendTo('body');
 
-  module('.icon()');
+  QUnit.specify('Ninja UI', function() {
+    describe('On jQuery ' + version, function() {
 
-    var icons = ['', 'arrow-down', 'arrow-right', 'camera', 'circle', 'circle-clear', 'circle-minus', 'circle-plus', 'go', 'home', 'mail', 'search', 'star', 'stop', 'triangle', 'warn'];
-    $.each(icons, function(i, icon) {
-      var $icon = $.ninja().icon({
-        name: icon
+      describe('Infrastructure', function() {
+        it('should load jQuery ' + version, function() {
+          assert($.fn.jquery).equals(version);
+        });
+
+        it('should load Ninja UI', function() {
+          assert($.ninja()).isDefined();
+        });
+
+        if (development) {
+          it('should return development Ninja UI version if in development mode', function() {
+            assert($.ninja().version()).equals('development');
+          });
+        }
       });
 
-      $examples.append($icon, ' ');
+      describe('.icon()', function() {
+        var icons = ['', 'arrow-down', 'arrow-right', 'camera', 'circle', 'circle-clear', 'circle-minus', 'circle-plus', 'go', 'home', 'mail', 'search', 'star', 'stop', 'triangle', 'warn'];
 
-      test('should have icon class', function () {
-        expect(1);
-        ok($icon.is('.ninja-icon'), '.ninja-icon');
+        $.each(icons, function(i, icon) {
+          var $icon = $.ninja().icon({
+            name: icon
+          });
+
+          $examples.append($icon, ' ');
+        });
+
+        $examples.append($.ninja().icon({
+          color: '#c00',
+          name: 'stop'
+        }), ' ', $.ninja().icon({
+          color: 'goldenrod',
+          name: 'warn'
+        }), ' ', $.ninja().icon({
+          color: 'green',
+          name: 'go'
+        }), '<br/>');
+
+        given(icons).
+        it('should have icon class', function(iconName) {
+          assert($.ninja().icon({name: iconName}).is('.ninja-icon')).isTrue();
+        });
+
+        given(icons).
+        it('should have aria label', function(iconName) {
+          assert($.ninja().icon({name: iconName}).attr('aria-label')).equals(iconName);
+        });
+
+        given(icons).
+        it('should have image role', function(iconName) {
+          assert($.ninja().icon({name: iconName}).attr('role')).equals('img');
+        });
+
+        given(icons).it('should have icon title', function(iconName) {
+          assert($('title', $.ninja().icon({name: iconName})).text()).equals(iconName);
+        });
       });
 
-      test('should be accessible', function () {
-        expect(3);
-        equal($icon.attr('aria-label'), icon, 'aria-label: ' + icon);
-        equal($icon.attr('role'), 'img', 'role: img');
-        equal($('title', $icon).text(), icon, 'title: ' + icon);
+      describe('.button()', function() {
+        var $button = $.ninja().button({
+          css: {
+            'margin-right': '16px'
+          },
+          html: '<b>New</b> Button'
+        }).appendTo($examples);
+
+        it('should have Ninja UI\'s default class', function() {
+          assert($button.hasClass('ninja')).isTrue();
+        });
+
+        it('should accept css overrides on creation', function() {
+          assert($button.css('margin-right')).equals('16px');
+          // Note that different browsers are not consistent in how they deal with invalid styles.
+          // Note also that values given and values returned do not always match, such as 1em returning 16px
+        });
+
+        it('should accept html content on creation', function() {
+          $('#qunit-fixture').append($.ninja().button({html: '<b>New</b> Button'}));
+
+          assert($button.html()).equals('<b>New</b> Button');
+        });
       });
+
+      
     });
-    $examples.append($.ninja().icon({
-      color: '#c00',
-      name: 'stop'
-    }), ' ', $.ninja().icon({
-      color: 'goldenrod',
-      name: 'warn'
-    }), ' ', $.ninja().icon({
-      color: 'green',
-      name: 'go'
-    }), '<br/>');
-
-  module('.button()');
-
-    var $button = $.ninja().button({
-      css: {
-        'margin-right': '16px'
-      },
-      html: '<b>New</b> Button'
-    }).appendTo($examples);
-
-    test('should have Ninja UI\'s default class', function () {
-      expect(1);
-      ok($button.hasClass('ninja'), '.ninja');
-    });
-
-    test('should accept css overrides on creation', function () {
-      expect(1);
-      equal($button.css('margin-right'), '16px', 'making them visible in jQuery\'s .css()');
-      // Note that different browsers are not consistent in how they deal with invalid styles.
-      // Note also that values given and values returned do not always match, such as 1em returning 16px
-    });
-
-    test('should accept html content on creation', function () {
-      expect(1);
-      $('#qunit-fixture').append($.ninja().button({html: '<b>New</b> Button'}));
-      equal($button.html(), '<b>New</b> Button', 'which jQuery can then render');
-    });
-
+  });
 });
