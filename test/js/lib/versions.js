7db23eb664d84d848038e5818dc0432115fc24b3
Faisal N. Jawdat <faisal@faisal.com>
2011-11-06 21:10:17 -0500

    for reference, noting source of code in versions.js

--- test/js/lib/versions.js
+++ test/js/lib/versions.js
@@ -1,4 +1,10 @@
 /**
+ * This is based on code from the jquery-versions project:
+ *   https://github.com/protonet/jquery-versions/
+ * and licensed under the therein.
+ */
+
+/**
  * $versions - Test your code/plugin in multiple jQuery versions
  * Works perfectly together with QUnit and other test libraries
  *
@@ -12,9 +18,13 @@
  *    });
  */
 var $versions = (function() {
-  // URL_TEMPLATE rewritten to only use cached local files for testing.
-  var URL_TEMPLATE = "js/lib/jquery/jquery-{version}.min.js";
-
+  
+  var // location.protocol can be file:, that why we either take http: or https:
+      protocol     = location.protocol == "https:" ? location.protocol : "http:",
+      // cache buster needed to bypass browser cache when loading most recent branch version
+      cacheBuster  = new Date().getTime(),
+      URL_TEMPLATE = protocol + "//ajax.googleapis.com/ajax/libs/jquery/{version}/jquery.min.js?" + cacheBuster;
+  
   var _makeArray = function(pseudoArray) {
     var array = [];
     if (pseudoArray && pseudoArray.length) {
@@ -24,13 +34,13 @@ var $versions = (function() {
     }
     return array;
   };
-
+  
   var _cloneArray = _makeArray;
-
+  
   var _isArray = function(array) {
     return Object.prototype.toString.call(array) === "[object Array]";
   };
-
+  
   var _loadScript = function(url, callback) {
     var script = document.createElement("script"),
         existingScript;
@@ -48,11 +58,11 @@ var $versions = (function() {
     script.onreadystatechange = function() {
       if (/complete|loaded/.test(script.readyState)) { script.onload(); }
     };
-
+    
     existingScript = document.getElementsByTagName("script")[0];
     existingScript.parentNode.insertBefore(script, existingScript);
   };
-
+  
   var _loadScripts = function(urls, callback) {
     urls = _cloneArray(urls);
     var loadNext = function() {
@@ -64,31 +74,31 @@ var $versions = (function() {
     };
     loadNext();
   };
-
+  
   var _loadJquery = function(version, callback) {
     var url = URL_TEMPLATE.replace("{version}", version);
     _loadScript(url, callback);
   };
-
-
+  
+  
   var VersionRunner = function(versions) {
     this.versions         = versions;
     this.scripts          = [];
     this.callbackMethods  = [];
     return this;
   };
-
+  
   VersionRunner.prototype.add = function(version) {
     this.versions.push(version);
     return this;
-  };
-
+  }; 
+  
   VersionRunner.prototype.load = function(scripts) {
     scripts = _isArray(scripts) ? scripts : _makeArray(arguments);
     this.scripts = this.scripts.concat(scripts);
     return this;
   };
-
+  
   VersionRunner.prototype.execute = function(testExecuter) {
     var versions        = _cloneArray(this.versions),
         that            = this,
@@ -99,7 +109,7 @@ var $versions = (function() {
                 var currentJquery  = window.jQuery,
                     version        = currentJquery.fn.jquery;
                 testExecuter(currentJquery, currentJquery, version);
-
+                
                 // Clean up afterwards
                 currentJquery.noConflict(true);
                 loadNextJquery();
@@ -111,16 +121,16 @@ var $versions = (function() {
             }
           }
         };
-
+    
     loadNextJquery();
     return this;
   };
-
+  
   VersionRunner.prototype.callback = function(method) {
     this.callbackMethods.push(method);
     return this;
   };
-
+  
   return function(versions) {
     versions = _isArray(versions) ? versions : _makeArray(arguments);
     return new VersionRunner(versions);
