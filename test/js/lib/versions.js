36a1786b632beca965852697f83afdb820d5d1d5
Faisal N. Jawdat <faisal@faisal.com>
2011-11-06 21:12:10 -0500

    Reverting breakage introduced in prior commit.

--- test/js/lib/versions.js
+++ test/js/lib/versions.js
@@ -18,13 +18,9 @@
  *    });
  */
 var $versions = (function() {
-  
-  var // location.protocol can be file:, that why we either take http: or https:
-      protocol     = location.protocol == "https:" ? location.protocol : "http:",
-      // cache buster needed to bypass browser cache when loading most recent branch version
-      cacheBuster  = new Date().getTime(),
-      URL_TEMPLATE = protocol + "//ajax.googleapis.com/ajax/libs/jquery/{version}/jquery.min.js?" + cacheBuster;
-  
+  // URL_TEMPLATE rewritten to only use cached local files for testing.
+  var URL_TEMPLATE = "js/lib/jquery/jquery-{version}.min.js";
+
   var _makeArray = function(pseudoArray) {
     var array = [];
     if (pseudoArray && pseudoArray.length) {
@@ -34,13 +30,13 @@ var $versions = (function() {
     }
     return array;
   };
-  
+
   var _cloneArray = _makeArray;
-  
+
   var _isArray = function(array) {
     return Object.prototype.toString.call(array) === "[object Array]";
   };
-  
+
   var _loadScript = function(url, callback) {
     var script = document.createElement("script"),
         existingScript;
@@ -58,11 +54,11 @@ var $versions = (function() {
     script.onreadystatechange = function() {
       if (/complete|loaded/.test(script.readyState)) { script.onload(); }
     };
-    
+
     existingScript = document.getElementsByTagName("script")[0];
     existingScript.parentNode.insertBefore(script, existingScript);
   };
-  
+
   var _loadScripts = function(urls, callback) {
     urls = _cloneArray(urls);
     var loadNext = function() {
@@ -74,31 +70,31 @@ var $versions = (function() {
     };
     loadNext();
   };
-  
+
   var _loadJquery = function(version, callback) {
     var url = URL_TEMPLATE.replace("{version}", version);
     _loadScript(url, callback);
   };
-  
-  
+
+
   var VersionRunner = function(versions) {
     this.versions         = versions;
     this.scripts          = [];
     this.callbackMethods  = [];
     return this;
   };
-  
+
   VersionRunner.prototype.add = function(version) {
     this.versions.push(version);
     return this;
-  }; 
-  
+  };
+
   VersionRunner.prototype.load = function(scripts) {
     scripts = _isArray(scripts) ? scripts : _makeArray(arguments);
     this.scripts = this.scripts.concat(scripts);
     return this;
   };
-  
+
   VersionRunner.prototype.execute = function(testExecuter) {
     var versions        = _cloneArray(this.versions),
         that            = this,
@@ -109,7 +105,7 @@ var $versions = (function() {
                 var currentJquery  = window.jQuery,
                     version        = currentJquery.fn.jquery;
                 testExecuter(currentJquery, currentJquery, version);
-                
+
                 // Clean up afterwards
                 currentJquery.noConflict(true);
                 loadNextJquery();
@@ -121,16 +117,16 @@ var $versions = (function() {
             }
           }
         };
-    
+
     loadNextJquery();
     return this;
   };
-  
+
   VersionRunner.prototype.callback = function(method) {
     this.callbackMethods.push(method);
     return this;
   };
-  
+
   return function(versions) {
     versions = _isArray(versions) ? versions : _makeArray(arguments);
     return new VersionRunner(versions);
